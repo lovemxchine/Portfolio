e6781e0b3275f942cce7c8294f067a957cfaf4b7
Channarong Chancher <143964932+lovemxchine@users.noreply.github.com>
2024-11-08 18:35:58 +0700

    content

--- src/components/introduce.js
+++ src/components/introduce.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 const Introduce = () => {
-  const text = "Hi ! My name is Noey";
+  const text = "Hi ! My12 name is Noey";
   const career = " I'm Web Developer";
 
   const [displayText, setDisplayText] = useState("");
